c83a224076077be51c9ee32960a27b7ab866be39
Sean Glancy <spglancy@gmail.com>
2019-08-11 06:07:42 -0700

    project done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      zip: '',
+      mood: '',
+      temp: null,
+      weather: null
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+    this.getWeather(this.state.zip)
+  }
+
+  getWeather(zip) {
+    const api = '36697159ee2bad94bfb8136b848fdb6b'
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${api}`
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.weather[0].main)
+        this.setState({ temp: res.main.temp, weather: res.weather[0].main })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="App" >
+          <input placeholder='Zip Code' onChange={e => this.setState({ zip: e.target.value })} value={this.state.zip} />
+          <input placeholder='Mood' onChange={(e) => this.setState({ mood: e.target.value })} value={this.state.mood} />
+          <button onClick={e => this.handleClick(e)}>Get Weather</button>
+        </div>
+        <div>
+          {this.state.temp === null ? null :
+            <div className='container'>
+              <p>Weather: {this.state.weather}</p>
+              <p>Temperature: {parseInt((this.state.temp - 273.15) * 9 / 5 + 32)}</p>
+              <p>Mood: {this.state.mood}</p>
+            </div>
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
